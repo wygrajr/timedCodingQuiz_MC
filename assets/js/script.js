6b828a1b6f4345d184459329fe666a8f7ba06f50
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-02 20:55:11 -0400

    Added Questions and answers for users to interact with

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 //1. When I click the start button, a series of questions begin
+var questionsAndAnswers=[ //<--- Array of Objects
+    {
+        question: "What does [] always denote in Javascript?",
+        choices: ["object","function","array","event"],
+        answer: "array"
+    },
+
+    {
+        question: "What Element is used to wrtie CSS & Javascript in HTML?",
+        choices: ["<meta>","<head>","<footer>","<script>"],
+        answer: "<script>"
+    },
+
+    {
+        question: "What is setInterval()?",
+        choices: ["funtion","variable","object","event"],
+        answer: "function"
+    },
+]
 //2. The timer begins to countdown when I start the quiz
 //3. When I select the correct answer, I move on to the next one, but when I select a wrong answer, time is also reduced from the countdown
 //4. The quiz ends when I answer all the questions or time reaches 0
