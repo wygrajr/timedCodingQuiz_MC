3caf0ecb2ae32601ca17dd763897cd3d97ef207f
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-02 20:26:18 -0400

    Added psuedo code algorithm

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//1. When I click the start button, a series of questions begin
+//2. The timer begins to countdown when I start the quiz
+//3. When I select the correct answer, I move on to the next one, but when I select a wrong answer, time is also reduced from the countdown
+//4. The quiz ends when I answer all the questions or time reaches 0
+//5. I can enter my initals and save my score at the end of the quiz
