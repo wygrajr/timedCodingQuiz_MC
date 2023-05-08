9c14467dcf005886c3df98a4d20866288048c20d
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-07 20:03:10 -0400

    Start the quiz, loop through the questions, react to what the user selects

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ var multipleChoices = document.querySelector("#choices");
 var submitButton = document.querySelector("#submit");
 var startButton = document.querySelector("#start");
 var initals = document.querySelector("#name");
-var results = document.querySelector("#result");
+var reaction = document.querySelector("#quizReaction");
 
 //2. The timer begins to countdown when I start the quiz
 
@@ -44,5 +44,46 @@ function quizStart() {
 }
 
 //3. When I select the correct answer, I move on to the next one, but when I select a wrong answer, time is also reduced from the countdown
+
+function quiz() {
+    var currentQuestion = questionsAndAnswers[currentQuestionIndex];
+  var promptEl = document.getElementById("questions")
+    promptEl.textContent = currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(function(choice, i) {
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("value", choice);
+        choiceButton.textContent = i + 1 + ". " + choice;
+        choiceButton.onclick = questionLoop;
+        multipleChoices.appendChild(choiceButton);
+    });
+}
+
+function questionLoop() {
+    if (this.value !== questionsAndAnswers[currentQuestionIndex].answer) {
+      time -= 10;
+      if (time < 0) {
+        time = 0;
+      }
+      countDown.textContent = time;
+      reaction.textContent = `Wrong! The correct answer was ${questionsAndAnswers[currentQuestionIndex].answer}.`;
+      reaction.style.color = "red";
+    } else {
+        reaction.textContent = "Correct!";
+        reaction.style.color = "green";
+    }
+    reaction.setAttribute("class", "reaction");
+    setTimeout(function() {
+        reaction.setAttribute("class", "reaction hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questionsAndAnswers.length) {
+      quizEnd();
+    } else {
+      quiz();
+    }
+}
 //4. The quiz ends when I answer all the questions or time reaches 0
+
+
 //5. I can enter my initals and save my score at the end of the quiz
